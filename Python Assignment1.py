6853ba0f44e996dea55f38a98083584a2103ba90
Hittloson <hittlo86@gmail.com>
2022-07-14 17:52:41 +0000

    Added Python Assignment.py to the repo

---
+++ Python Assignment1.py
@@ -0,0 +1,11 @@
+Name=input("Enter your name: ")
+Age=int(input("Enter your age: "))
+currentYear=int(2022)
+x=currentYear - Age
+def sayMyAge():
+    print("Hello," , Name , "You are born in the year", x)
+
+sayMyAge()
+
+
+
